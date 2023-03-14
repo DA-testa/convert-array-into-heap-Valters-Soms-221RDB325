b6c2c64dad0e43c2dc37e22096c207dbf63e4fb1
Valters-Soms-221RDB325 <124680753+Valters-Soms-221RDB325@users.noreply.github.com>
2023-03-14 18:12:21 +0000

    <1>

--- build_heap.py
+++ build_heap.py
@@ -1,41 +1,80 @@
 # python3
 
-
 def build_heap(data):
+    n = len(data)
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
 
+    def heapify(i):
+        largest = i
+        left = 2*i + 1
+        right = 2*i + 2
+
+        if left < n and data[left] > data[largest]:
+            largest = left
+
+        if right < n and data[right] > data[largest]:
+            largest = right
+
+        if largest != i:
+            swaps.append((i, largest))
+            data[i], data[largest] = data[largest], data[i]
+            heapify(largest)
+
+    for i in range(n//2, -1, -1):
+        heapify(i)
 
     return swaps
 
 
 def main():
     
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
+    input_text = input()
+    if 'F' in input_text:
+        input_file = input()
+        input_file = "test/" + input_file
+        if 'a' not in input_file:
+            try:
+                with open(input_file, "r") as f:
+                    n = int(f.readline())          
+                    data = list(map(int, f.readline().split()))
 
+                    # checks if lenght of data is the same as the said lenght
+                    assert len(data) == n
 
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+                    # calls function to assess the data 
+                    # and give back all swaps
+                    swaps = build_heap(data)
 
-    # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
+                    # TODO: output how many swaps were made, 
+                    # this number should be less than 4n (less than 4*len(data))
 
-    # calls function to assess the data 
-    # and give back all swaps
-    swaps = build_heap(data)
+                    # output all swaps
+                    print(len(swaps))
+                    for i, j in swaps:
+                        print(i, j)
 
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
+            except FileNotFoundError:
+                return print("File_not_found_error")
 
+    if 'I' in input_text:
+        # input from keyboard
+        n = int(input())
+        data = list(map(int, input().split()))
 
-    # output all swaps
-    print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
+        # checks if lenght of data is the same as the said lenght
+        assert len(data) == n
+
+        # calls function to assess the data 
+        # and give back all swaps
+        swaps = build_heap(data)
+
+        # TODO: output how many swaps were made, 
+        # this number should be less than 4n (less than 4*len(data))
+
+        # output all swaps
+        print(len(swaps))
+        for i, j in swaps:
+            print(i, j)
 
 
 if __name__ == "__main__":
